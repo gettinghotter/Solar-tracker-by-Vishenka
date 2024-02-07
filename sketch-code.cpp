169a5f2f5beead9579e788a33db5c31131135dce
gettinghotter <gamingakk19@gmail.com>
2024-02-07 18:30:30 +0300

    code logic commented

--- sketch-code.cpp
+++ sketch-code.cpp
@@ -1,10 +1,13 @@
+// подключение библиотеки серво
 #include <Servo.h>
 
+// введение переменных, считывающих показания пинов, для дальнейших расчетов
 const int photoPin0 = A0;
 const int photoPin1 = A1;
 const int photoPin2 = A2;
 const int photoPin3 = A3;
 
+// переменные для двух серво
 Servo servoH;
 int servohValue = 90;
 Servo servoV;
@@ -12,6 +15,8 @@ int servovValue = 90;
 
 void setup() 
 {
+  //подключение пинов к переменным и первичная настройка
+
   Serial.begin(9600);
   servoH.attach(9);
   servoV.attach(10);
@@ -20,27 +25,38 @@ void setup()
 
 void loop()
 {
-  // pin 0 - horisontal left
-  // pin 2 - horisontal right
-  // pin 1 - vertical left
-  // pin 3 - vertical right
+  // pin 0 - горизонтальный левый
+  // pin 2 - горизонтальный правый
+  // pin 1 - вертикальный левый
+  // pin 3 - вертикальный правый
+
+
+  // постоянное чтение значений фоторезисторов
 
   int photoValue0 = analogRead(photoPin0);
   int photoValue1 = analogRead(photoPin1);
   int photoValue2 = analogRead(photoPin2);
   int photoValue3 = analogRead(photoPin3);
 
+  // введение переменных для двух потенциометров(dtime - для скорости перемещения серво, ...)
+
   int dtime = analogRead(4)/20;
   int tol = analogRead(5)/4;
 
-  int avgTop = (photoValue1 + photoValue3) / 2;     // average value top
-  int avgBottom = (photoValue0 + photoValue2) / 2;  // average value bottom
-  int avgLeft = (photoValue1 + photoValue0) / 2;    // average value left
-  int avgRight = (photoValue3 + photoValue2) / 2;   // average value right
-  
-  int diffVert = avgTop - avgBottom;   // diffirence of top and bottom
-  int diffHoris = avgLeft - avgRight;  // diffirence of left and right
+  // усреднение значений из всех фоторезисторов
 
+  int avgTop = (photoValue1 + photoValue3) / 2;     // среднее значение сверху
+  int avgBottom = (photoValue0 + photoValue2) / 2;  // среднее значение снизу
+  int avgLeft = (photoValue1 + photoValue0) / 2;    // среднее значение слева
+  int avgRight = (photoValue3 + photoValue2) / 2;   // среднее значение справа
+
+  // разница между усредненными значениями
+  
+  int diffVert = avgTop - avgBottom;   // разница между верхним и нижним
+  int diffHoris = avgLeft - avgRight;  // разница между левым и правым
+
+
+  // условие для движения вертикального серво
   if (-1 * tol > diffVert || diffVert > tol)
   {
     if (avgTop > avgBottom)
@@ -59,10 +75,13 @@ void loop()
         servovValue = 0;
       }
     }
+    // постоянное присваивание значений в серво
     servoV.write(servovValue);
   }
 
-  if (-1*tol > diffHoris || diffHoris > tol)
+  // условие для движения горизонтального серво
+
+  if (-1 * tol > diffHoris || diffHoris > tol)
   {
     if (avgLeft > avgRight)
     {
@@ -82,8 +101,9 @@ void loop()
     }
     else if (avgLeft == avgRight)
     {
-      // nothing
     }
+
+    // постоянное присваивание значений в серво
     servoH.write(servohValue);
   }
   delay(dtime);
